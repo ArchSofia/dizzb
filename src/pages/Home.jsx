26a8a708a71d4c1021309d4160b4cf280c43353a
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-22 18:04:31 -0300

    Agrega estructura basica navbar body footer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Body from "../components/Body/Body";
+
+const Home = () => {
+	return (
+		<div>
+			<Navbar />
+			<Body />
+			<Footer />
+		</div>
+	);
+};
+
+export default Home;
