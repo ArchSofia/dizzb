d2636f2ded522ebad5c4caac3c12ba5e0d42bf77
ArchSofia <sofiadiazvaldez@gmail.com>
2023-09-22 22:20:50 -0300

    Agrega Navbar version desktop

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
 const Navbar = () => {
-	return <div className="navbar">Navbar</div>;
+	return (
+		<div className="navbar">
+			<div className="logo">
+				<img src={logo} className="dizzb" alt="Dizzb logo" />
+			</div>
+			<nav className="navigation-menu">
+				<ul>
+					<li>
+						<a href="#whydizzb" className="menu-button">
+							Why Dizzb
+						</a>
+					</li>
+					<li>
+						<a href="#solutions" className="menu-button">
+							Solutions
+						</a>
+					</li>
+					<li>
+						<a href="#experence" className="menu-button">
+							Our experience
+						</a>
+					</li>
+					<li>
+						<a href="#partnerships" className="menu-button">
+							Partnerships
+						</a>
+					</li>
+				</ul>
+				<button className="button">Get started</button>
+			</nav>
+		</div>
+	);
 };
 
 export default Navbar;
